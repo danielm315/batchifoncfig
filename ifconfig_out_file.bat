48fe662b5f95c6603529acfcfb1c04e3d7db8b12
dmattress <126580500+danielm315@users.noreply.github.com>
2024-09-19 22:32:28 -0400

    Update ifconfig_out_file.bat

--- ifconfig_out_file.bat
+++ ifconfig_out_file.bat
@@ -2,9 +2,10 @@
 title made by danielm315
 chcp 437 >nul
 
-set /p choice=!
+
 
 echo going to wsl and running ifconfig...
 
 :wsl
 powershell.exe wsl ifconfig
+echo writing results to ifconfig_wsl.txt
